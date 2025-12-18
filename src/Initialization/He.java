bb79a073f4d7f3b81b61f1b4a6ff34f1f1ee6f0b
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2025-12-18 08:10:43 +0200

    Create He.java

---
+++ src/Initialization/He.java
@@ -0,0 +1,21 @@
+package Initialization;
+
+import java.util.Random;
+
+public class He implements WeightInitializer {
+
+    private final Random random = new Random();
+
+    @Override
+    public double[] init(int nInput, int nOutput) {
+        double[] weights = new double[nInput * nOutput];
+
+        double limit = Math.sqrt(6.0 / nInput);
+
+        for (int i = 0; i < weights.length; ++i) {
+            weights[i] = (random.nextDouble() * 2 * limit) - limit;
+        }
+
+        return weights;
+    }
+}
