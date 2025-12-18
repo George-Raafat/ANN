4247a7301f19535f2e8ea749a769190d2577d081
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2025-12-18 08:12:27 +0200

    Update Xavier.java

--- src/Initialization/Xavier.java
+++ src/Initialization/Xavier.java
@@ -10,7 +10,6 @@ public class Xavier implements WeightInitializer {
     public double[] init(int nInput, int nOutput) {
         double[] weights = new double[nInput * nOutput];
 
-
         double limit = Math.sqrt(6.0 / (nInput + nOutput));
 
         for (int i = 0; i < weights.length; ++i) {
